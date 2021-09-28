1cbe025427f9b7fe118b53a4c63df8701da9418d
rojenbasnet <90825611+StellarAlpha@users.noreply.github.com>
2021-09-28 08:23:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+const country = 'Nepal';
+const continent = 'Asia';
+const populationMillion = 30.314998;
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
